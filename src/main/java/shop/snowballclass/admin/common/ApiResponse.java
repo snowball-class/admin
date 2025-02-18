151b676d9059ed5dfaef97c611f90a5de86cbc20
Chanui <jcu011@naver.com>
2025-02-18 16:04:27 +0900

    refactor: ApiResponse status int형으로 변경

--- src/main/java/shop/snowballclass/admin/common/ApiResponse.java
+++ src/main/java/shop/snowballclass/admin/common/ApiResponse.java
@@ -4,19 +4,19 @@ package shop.snowballclass.admin.common;
 import org.springframework.http.HttpStatus;
 
 public record ApiResponse<T>(
-        HttpStatus status,
+        int status,
         String message,
         T data
 ) {
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(HttpStatus.OK, null, data);
+        return new ApiResponse<>(HttpStatus.OK.value(), null, data);
     }
 
     public static <T> ApiResponse<T> created(T data) {
-        return new ApiResponse<>(HttpStatus.CREATED, null, data);
+        return new ApiResponse<>(HttpStatus.CREATED.value(), null, data);
     }
 
     public static <T> ApiResponse<T> accepted(T data) {
-        return new ApiResponse<>(HttpStatus.ACCEPTED, null, data);
+        return new ApiResponse<>(HttpStatus.ACCEPTED.value(), null, data);
     }
 }
