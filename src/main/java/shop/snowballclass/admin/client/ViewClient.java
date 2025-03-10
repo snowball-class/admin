486af46c92c280baad9595b4104c3973ea1e40a0
Chanui <jcu011@naver.com>
2025-03-10 19:31:22 +0900

    feat: ViewClient /lessons/bulk API 추가

--- src/main/java/shop/snowballclass/admin/client/ViewClient.java
+++ src/main/java/shop/snowballclass/admin/client/ViewClient.java
@@ -1,14 +1,19 @@
 package shop.snowballclass.admin.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import shop.snowballclass.admin.dto.ApiResponse;
 import shop.snowballclass.admin.dto.client.EventLessonCreateRequest;
+import shop.snowballclass.admin.dto.client.EventLessonResponse;
+
+import java.util.List;
 
 @FeignClient(name = "view-service", url = "${feign.snowball.view}")
 public interface ViewClient {
     @PostMapping("/events/{eventId}/lessons")
     ApiResponse createEventLessons(@PathVariable("eventId") Long eventId, @RequestBody EventLessonCreateRequest lessonIds);
+
+    @GetMapping("/lessons/bulk")
+    ApiResponse<List<EventLessonResponse>> getEventLessonListByLessonIds(@RequestParam("ids") String lessonIds);
+
 }
